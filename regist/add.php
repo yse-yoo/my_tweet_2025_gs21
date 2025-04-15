2cd4c38c85099d7068db8f7bcfb13745f4b74ce8
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-04-15 13:41:34 +0900

    update

--- regist/add.php
+++ regist/add.php
@@ -11,6 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 // POSTデータ取得
 $posts = $_POST;
+// var_dump($posts);
+// exit;
 
 // TODO: セッションの APP_KEY 下の regist にPOSTデータを保存
 $_SESSION[APP_KEY]['regist'] = $posts;
@@ -20,13 +22,19 @@ $_SESSION[APP_KEY]['regist'] = $posts;
 // TODO: Userモデルでユーザ登録(insert)を実行
 $auth_user = [];
 // TODO: 仮のユーザIDを設定
-// $auth_user['id'] = 1; 
+if ($posts['account_name'] === 'test') {
+    $auth_user['id'] = 1; // 仮のユーザID
+} else {
+    $auth_user['id'] = null;
+}
 
 if (empty($auth_user['id'])) {
     // TODO: エラーを APP_KEY > errors > public セッションに保存
     // TODO: 入力画面(input.php)にリダイレクト
+    header('Location: input.php');
     exit;
 } else {
     // TODO: 登録成功の場合は完了画面(login/)へリダイレクト
+    header('Location: ../login/');
     exit;
 }
