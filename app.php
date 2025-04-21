d021108d179e4388b69a23d4e24c9f27379c0ee2
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-04-21 10:47:35 +0900

    update

--- app.php
+++ app.php
@@ -14,9 +14,20 @@ const LIB_DIR = __DIR__ . "/lib/";
 // components/ ディレクトリパス
 const COMPONENT_DIR = __DIR__ . "/components/";
 
+// upload image base path
+const UPLOADS_BASE = "images/uploads/";
+// profile image base path
+const PROFILE_BASE = "images/profile/";
+// upload image dir
+const UPLOADS_DIR = __DIR__ . UPLOADS_BASE;
+// profile image dir
+const PROFILE_DIR = __DIR__ . PROFILE_BASE;
+
 // ライブラリ読み込み
 require_once LIB_DIR . 'Database.php';
 require_once LIB_DIR . 'Sanitize.php';
+require_once LIB_DIR . 'File.php';
 
 // モデルクラスの読み込み
 require_once APP_DIR . 'models/User.php';
+require_once APP_DIR . 'models/Tweet.php';
